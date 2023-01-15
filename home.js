a8098c71bc3d998d302892151041a1f6f4a90a60
cloudyishere <115741943+cloudyishere@users.noreply.github.com>
2023-01-15 09:18:08 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -7,11 +7,66 @@ window.addEventListener("load", () => {
       document.body.removeChild(loader);
     });
   });
-  
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "150px";
+// Form validation
+var form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var name = document.querySelector("#name");
+  var email = document.querySelector("#email");
+  var cake = document.querySelector("#cake");
+  var message = document.querySelector("#message");
+
+  if (name.value === "" || email.value === "" || cake.value === "") {
+    alert("Please fill out all the required fields!");
+  } else {
+    alert("Thank you for your order! We will contact you shortly.");
+    form.reset();
   }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+});
+
+// Slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// Modal
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
